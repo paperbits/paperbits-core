f97b83bcbe86a0983141096c8778a4d20944fd74
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-01-11 11:52:43 -0800

    cleanup

--- src/gtm/ko/gtm.ts
+++ src/gtm/ko/gtm.ts
@@ -1,27 +1,27 @@
 import * as ko from "knockout";
 import template from "./gtm.html";
-import { IGtmConfig } from "@paperbits/common/sites/ISettings";
-import { ISettingsProvider } from '@paperbits/common/configuration/ISettingsProvider';
-import { Component } from "@paperbits/common/ko/decorators";
-   
+import { IGtmConfig } from "@paperbits/common/sites";
+import { ISettingsProvider } from "@paperbits/common/configuration";
+import { Component, OnMounted } from "@paperbits/common/ko/decorators";
+
 @Component({
     selector: "paperbits-gtm",
     template: template,
     injectable: "gtm"
 })
 export class GoogleTagManager {
-    static gtmDataLayerName = 'dataLayer';
+    public static gtmDataLayerName = "dataLayer";
     public gtmBootstrapper: KnockoutObservable<string>;
 
-    constructor(settingsProvider: ISettingsProvider) {
-        this.boot = this.boot.bind(this);
-        this.onConfigLoaded = this.onConfigLoaded.bind(this);
-
-        settingsProvider.getSettings().then(this.onConfigLoaded);
+    constructor(private readonly settingsProvider: ISettingsProvider) {
+        this.initialize = this.initialize.bind(this);
         this.gtmBootstrapper = ko.observable<string>();
     }
 
-    private onConfigLoaded(config: any): void {
+    @OnMounted()
+    private async initialize(): Promise<void> {
+        const config: any = await this.settingsProvider.getSettings();
+
         if (!config || !config.gtm) {
             return;
         }
@@ -30,15 +30,15 @@ export class GoogleTagManager {
     }
 
     private boot(gtm: IGtmConfig): void {
-        if (gtm.containerId == null){
+        if (gtm.containerId === null) {
             return;
         }
-        
+
         if (!gtm.dataLayerName) {
-            gtm.dataLayerName = 'dataLayer'
+            gtm.dataLayerName = "dataLayer";
         }
 
-        let bootstrapper = "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});let f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','" + gtm.dataLayerName + "','" + gtm.containerId + "');</script><noscript><iframe src='http://www.googletagmanager.com/ns.html?id=" + gtm.containerId + "'  height='0' width='0' style='display:none;visibility:hidden'></iframe> </noscript>";
+        const bootstrapper = "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});let f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','" + gtm.dataLayerName + "','" + gtm.containerId + "');</script><noscript><iframe src='http://www.googletagmanager.com/ns.html?id=" + gtm.containerId + "'  height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>";
 
         this.gtmBootstrapper(bootstrapper);
     }
