4a04f2735fc8c51bc78e5c548635cd317b3509bb
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-12-21 10:00:27 -0800

    Added jsdocs to contracts and models.

--- src/map/mapContract.ts
+++ src/map/mapContract.ts
@@ -1,8 +1,24 @@
 import { Contract } from "@paperbits/common";
 
 export interface MapContract extends Contract {
+    /**
+     * 
+     */
     location: string;
+
+
+    /**
+     * 
+     */
     layout?: string;
+
+    /**
+     * Pin caption.
+     */
     caption?: string;
+
+    /**
+     * Indicates, whether zoom controls need to be displayed.
+     */
     zoomControl?: string;
 }
