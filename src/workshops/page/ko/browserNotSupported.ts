ba3af38ddaae4da31ab4deda61576ae34bcb0e9b
Tuan Nguyen <atuannguyen1101@gmail.com>
2020-06-28 15:29:40 -0400

    Change name

--- src/workshops/page/ko/browserNotSupported.ts
+++ src/workshops/page/ko/browserNotSupported.ts
@@ -1,10 +1,10 @@
 import { Component } from "@paperbits/common/ko/decorators";
 
 @Component({
-    selector: "invalid-browser",
+    selector: "browser-not-supported",
     template: "<div><h1>The app does not support this version of your browser, please update or use a different browser.</h1></div>"
 })
-export class InvalidBrowser {
+export class BrowserNotSupported {
     constructor() {
     }
 }
