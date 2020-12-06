d18277412b304ab946d7e2dc091fbcc3d6a4d5e3
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-12-05 20:17:49 -0800

    Cleaned up placeholder widget.

--- src/placeholder/ko/placeholderViewModelBinder.ts
+++ src/placeholder/ko/placeholderViewModelBinder.ts
@@ -4,15 +4,7 @@ import { PlaceholderViewModel } from "./placeholderViewModel";
 
 export class PlaceholderViewModelBinder implements ViewModelBinder<PlaceholderModel, PlaceholderViewModel> {
     public async modelToViewModel(model: PlaceholderModel): Promise<PlaceholderViewModel> {
-        const viewModel = new PlaceholderViewModel(model.message);
-
-        const binding = {
-            flow: "contents"
-        };
-
-        viewModel["widgetBinding"] = binding;
-
-        return viewModel;
+        return new PlaceholderViewModel(model.message);
     }
     public canHandleModel(model: PlaceholderModel): boolean {
         return model instanceof PlaceholderModel;
