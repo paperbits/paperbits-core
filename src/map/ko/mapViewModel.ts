bcd81075d434073cc826cd2d6124de7f4e7f2f0b
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-12-08 23:25:50 -0800

    Renamed Map widget component.

--- src/map/ko/mapViewModel.ts
+++ src/map/ko/mapViewModel.ts
@@ -4,7 +4,7 @@ import { Component } from "@paperbits/common/ko/decorators";
 import { StyleModel } from "@paperbits/common/styles";
 
 @Component({
-    selector: "paperbits-googleMaps",
+    selector: "map",
     template: template
 })
 export class MapViewModel {
