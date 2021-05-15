38624b24835562e518f6fa7bcb942f36d3612850
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-05-15 16:31:57 -0700

    Fixed map binding handler imports.

--- src/map/map.publish.module.ts
+++ src/map/map.publish.module.ts
@@ -1,7 +1,7 @@
-import "./bindingHandlers.googlemap";
+import "./ko/bindingHandlers.googlemap";
 import { IInjectorModule, IInjector } from "@paperbits/common/injection";
-import { MapViewModel } from "./ko/mapViewModel";
 import { MapModelBinder } from "./mapModelBinder";
+import { MapViewModel } from "./ko/mapViewModel";
 import { MapViewModelBinder } from "./ko/mapViewModelBinder";
 
 
