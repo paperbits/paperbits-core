87e2eab9e61ade8d52d53c1ae8f4bd46938f0cc3
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-01-15 21:33:36 -0800

    Removed automatic KO bootstrapping.

--- src/ko/knockout.design.module.ts
+++ src/ko/knockout.design.module.ts
@@ -1,25 +1,10 @@
-import * as ko from "knockout";
 import { IInjector, IInjectorModule } from "@paperbits/common/injection";
 import { KnockoutValidation } from "./validation";
-
-
-class KnockoutBootstrapper {
-    constructor() {
-        this.bootstrap();
-    }
-
-    public bootstrap(): void {
-        document.addEventListener("DOMContentLoaded", () => {
-            setImmediate(() => ko.applyBindings(undefined, undefined));
-        });
-    }
-}
 
 export class KnockoutDesignModule implements IInjectorModule {
     public register(injector: IInjector): void {
         // const componentBinders = injector.resolve<Bag<ComponentBinder>>("componentBinders");
         // componentBinders["knockout"] = new KnockoutComponentBinder();
-        injector.bindToCollection("autostart", KnockoutBootstrapper);
         injector.bindToCollection("autostart", KnockoutValidation);
     }
 }
