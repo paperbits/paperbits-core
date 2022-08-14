76104d9f71a67ec6cbc93e9eec9202bf3d3ea046
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-08-14 16:22:51 -0700

    Moved auth context from Tray to the ToolButton.

---
+++ src/workshops/authContextToolbutton.ts
@@ -0,0 +1,41 @@
+import * as ko from "knockout";
+import { IComponent, ToolButton, ViewManager } from "@paperbits/common/ui";
+import { RoleModel, RoleService } from "@paperbits/common/user";
+
+
+export class AuthContextToolButton implements ToolButton {
+    public readonly selectedRoles: ko.ObservableArray<RoleModel> = ko.observableArray();
+    public readonly availableRoles: ko.ObservableArray<RoleModel>= ko.observableArray();
+    public iconClass: string = "paperbits-icon paperbits-icon paperbits-single-02";
+    public title: string = "Roles";
+    public tooltip: string = `<h1>Roles</h1><p>Push your changes to the storage.</p><div class="subtle">(Ctrl+S)</div>`;
+    public disabled: ko.Observable<boolean>;
+
+    public component: IComponent = {
+        name: 'role-selector',
+        params: {
+            availableRoles: this.availableRoles,
+            selectedRoles: this.selectedRoles,
+            onSelect: this.onRoleSelect.bind(this)
+        }
+    }
+   
+    constructor(
+        private readonly roleService: RoleService,
+        private readonly viewManager: ViewManager
+    ) {
+        this.disabled = ko.observable(false);
+        this.initialize();
+    }
+
+    private async initialize(): Promise<void> {
+        const roles = await this.roleService.getRoles();
+        this.availableRoles(roles.slice(1)); // Excluding Everyone.
+        this.selectedRoles(this.viewManager.getViewRoles())
+    }
+
+    public onRoleSelect(roles: RoleModel[]): void {
+        this.selectedRoles(roles);
+        this.viewManager.setViewRoles(roles);
+    }
+}
--- src/workshops/tray/tray.ts
+++ src/workshops/tray/tray.ts
@@ -2,7 +2,6 @@ import * as ko from "knockout";
 import template from "./tray.html";
 import { Component, OnMounted } from "@paperbits/common/ko/decorators";
 import { ToolButton, ViewManager } from "@paperbits/common/ui";
-import { RoleModel, RoleService } from "@paperbits/common/user";
 import { ISettingsProvider } from "@paperbits/common/configuration";
 
 
@@ -12,35 +11,20 @@ import { ISettingsProvider } from "@paperbits/common/configuration";
 })
 export class Tray {
     public readonly buttons: ko.ObservableArray<ToolButton>;
-    public readonly selectedRoles: ko.ObservableArray<RoleModel>;
-    public readonly availableRoles: ko.ObservableArray<RoleModel>;
     public readonly localizationEnabled: ko.Observable<boolean>;
 
     constructor(
         private readonly trayCommands: ToolButton[],
-        private readonly roleService: RoleService,
-        private readonly viewManager: ViewManager,
         private readonly settingsProvider: ISettingsProvider
     ) {
         this.buttons = ko.observableArray<ToolButton>(trayCommands);
-        this.availableRoles = ko.observableArray();
-        this.selectedRoles = ko.observableArray();
         this.localizationEnabled = ko.observable();
     }
 
     @OnMounted()
     public async initialize(): Promise<void> {
         this.buttons(this.trayCommands);
-        const roles = await this.roleService.getRoles();
-        this.availableRoles(roles.slice(1)); // Excluding Everyone.
-        this.selectedRoles(this.viewManager.getViewRoles());
-
         const localizationEnabled = await this.settingsProvider.getSetting<boolean>("features/localization");
         this.localizationEnabled(!!localizationEnabled);
-    }
-
-    public onRoleSelect(roles: RoleModel[]): void {
-        this.selectedRoles(roles);
-        this.viewManager.setViewRoles(roles);
     }
 }
