4d51c9efd934388535d1f5625b03a88f275815af
Rafał Mielowski <rmielowski@microsoft.com>
2022-09-01 17:07:42 +0200

    Add index.ts

---
+++ src/security/index.ts
@@ -0,0 +1,7 @@
+export * from "./baseVisibilityContextCommandProvider";
+export * from "./popupSecurityModelEditorProvider";
+export * from "./roleBasePopupSecurityModelEditorProvider";
+export * from "./security.design.module";
+export * from "./security.module";
+export * from "./securityModelEditor";
+export * from "./visibilityContextCommandProvider";
