6836931de6c1ade62ef3c7f72433002ec9578c1d
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2023-03-11 13:09:11 -0800

    Added "activate" behavior.

--- src/ko/bindingHandlers/bindingHandlers.activate.ts
+++ src/ko/bindingHandlers/bindingHandlers.activate.ts
@@ -3,8 +3,13 @@ import { ActivateBehavior } from "@paperbits/common/behaviors/behavior.activate"
 
 
 ko.bindingHandlers["activate"] = {
-    init: (element: HTMLElement, valueAccessor: () => (data: any) => void, allBindings, viewModel) => {
+    init: (element: HTMLElement, valueAccessor: () => (data: unknown) => void, allBindings, viewModel) => {
         const onActivate = valueAccessor();
+
+        if (!onActivate) {
+            console.warn(`Callback function for binding handler "activate" in undefined.`);
+            return null;
+        }
 
         const handle = ActivateBehavior.attach(element, {
             onActivate: onActivate.bind(viewModel),
@@ -13,40 +18,6 @@ ko.bindingHandlers["activate"] = {
 
         ko.utils.domNodeDisposal.addDisposeCallback(element, () => {
             handle.detach();
-        });
-
-        // const onActivate = valueAccessor();
-
-        // if (!onActivate) {
-        //     console.warn(`Callback function for binding handler "activate" in undefined.`);
-        //     return;
-        // }
-
-        // const data = ko.dataFor(element);
-        // const callback = onActivate.bind(viewModel);
-
-        // const onClick = (event: PointerEvent) => {
-        //     event.preventDefault();
-        //     event.stopImmediatePropagation();
-        //     callback(data);
-        // };
-
-        // const onKeyDown = (event: KeyboardEvent): void => {
-        //     if (event.key !== Keys.Enter && event.key !== Keys.Space) {
-        //         return;
-        //     }
-
-        //     event.preventDefault();
-        //     event.stopImmediatePropagation();
-        //     callback(data);
-        // };
-
-        // element.addEventListener(Events.KeyDown, onKeyDown);
-        // element.addEventListener(Events.Click, onClick);
-
-        // ko.utils.domNodeDisposal.addDisposeCallback(element, () => {
-        //     element.removeEventListener(Events.Click, onClick);
-        //     element.removeEventListener(Events.KeyDown, onKeyDown);
-        // });
+        })
     }
 };
