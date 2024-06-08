336d0bc11c0166018727df707ee3ced9c540f034
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2024-06-08 15:44:17 -0700

    Fixed Search widget registration.

--- src/search/search.publish.module.ts
+++ src/search/search.publish.module.ts
@@ -2,6 +2,9 @@ import { IInjectorModule, IInjector } from "@paperbits/common/injection";
 import { SearchViewModel } from "./ko/search";
 import { SearchModelBinder } from "./searchModelBinder";
 import { SearchViewModelBinder } from "./ko/searchViewModelBinder";
+import { IWidgetService } from "@paperbits/common/widgets";
+import { SearchInputModel } from "./searchInputModel";
+import { KnockoutComponentBinder } from "../ko";
 
 
 /**
@@ -9,8 +12,18 @@ import { SearchViewModelBinder } from "./ko/searchViewModelBinder";
  */
 export class SearchPublishModule implements IInjectorModule {
     public register(injector: IInjector): void {        
-        injector.bind("widget", SearchViewModel);
-        injector.bindToCollection("modelBinders", SearchModelBinder);
-        injector.bindToCollection("viewModelBinders", SearchViewModelBinder);
+        injector.bind("search", SearchViewModel);
+        injector.bindSingleton("searchModelBinder", SearchModelBinder);
+        injector.bindSingleton("searchViewModelBinder", SearchViewModelBinder);
+
+        const widgetService = injector.resolve<IWidgetService>("widgetService");
+
+        widgetService.registerWidget("search", {
+            modelDefinition: SearchInputModel,
+            componentBinder: KnockoutComponentBinder,
+            componentDefinition: SearchViewModel,
+            modelBinder: SearchModelBinder,
+            viewModelBinder: SearchViewModelBinder
+        });
     }
 }
