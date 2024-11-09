3564bfc0a53d9ada6cc7aef358b0271c4120d903
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2024-11-09 12:52:14 -0800

    Rewritten livearea binding handler into behavior.

--- src/ko/bindingHandlers/bindingHandlers.livearea.ts
+++ src/ko/bindingHandlers/bindingHandlers.livearea.ts
@@ -1,20 +1,16 @@
-import { EventManager, Events } from "@paperbits/common/events";
+import { LiveAreaBehavior } from "@paperbits/common/behaviors/liveAreaBehavior";
+import { EventManager } from "@paperbits/common/events";
 import * as ko from "knockout";
-
 
 export class LiveAreaBindingHandler {
     constructor(eventManager: EventManager) {
         ko.bindingHandlers["livearea"] = {
-            init: (element: HTMLElement, valueAccessor: () => (data: unknown) => void, allBindings, viewModel) => {
-                const notificationHandler = (notification: string) => {
-                    element.innerText = notification;
-                }
-
-                eventManager.addEventListener(Events.NotificationRequest, notificationHandler);
+            init: (element: HTMLElement) => {
+                const behaviorHandle = LiveAreaBehavior.attach(element, eventManager);
 
                 ko.utils.domNodeDisposal.addDisposeCallback(element, () => {
-                    eventManager.removeEventListener(Events.NotificationRequest, notificationHandler);
-                })
+                    behaviorHandle.detach();
+                });
             }
         };
     }
